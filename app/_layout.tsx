63cd2a1076e4d341e663b2410d8291241c9b7d43
Sivianny Vega <rvegamor@ccss.sa.cr>
2025-01-24 18:57:00 -0600

    Esto fue lo último antes del viaje a Europa

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,19 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { Text, View } from 'react-native'
 import 'react-native-reanimated';
-
+import "../global.css";
+import {GestureHandlerRootView  } from 'react-native-gesture-handler'
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background')
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -28,12 +33,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text className='mt-10 text-3xl text-light-text dark:text-dark-text'>Hola mundo</Text>
+        </ThemedView>
+        {/*<Stack>
+        </Stack>*/}
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
